843945de1f10c7dba31bfaac35fc959ee17ca75c
Dbwalya <danielbwalya620@gmail.com>
2024-08-21 09:35:46 +0200

    started working on the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Language from './Components/Language/Language'
+import Footer from "./Components/Footer.jsx";
 
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Language/>
+        <Navbar/>
+        <Hero/>
+        <About/>
+        <Language/>
+        <Footer />
     </div>
   )
 }
