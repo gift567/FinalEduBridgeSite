e2f72fd0517045426abbebdb75487056f5253db1
Dbwalya <danielbwalya620@gmail.com>
2024-08-21 13:59:39 +0200

    made the about section mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
-import Language from './Components/Language/Language'
+import Language from "./Components/Social/Social.jsx";
+import Footer from "./Components/Footer.jsx";
 
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Language/>
+        <Navbar/>
+        <Hero/>
+        <About/>
+        <Language/>
+        <Footer />
     </div>
   )
 }
