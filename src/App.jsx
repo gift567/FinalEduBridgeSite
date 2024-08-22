09b72eb55c0af462601722197245df26dcab9701
gift kateule <giftkateule719@gmail.com>
2024-08-22 17:37:15 +0200

    i have created the  social enterprise,contact Us,workstation components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
-import Language from './Components/Language/Language'
+<<<<<<< HEAD
+import Products from './Components/Products/Products'
+import Social from './Components/Social/Social'
+import Footer from './Components/Footer'
+=======
+import Language from "./Components/Social/Social.jsx";
+import Footer from "./Components/Footer.jsx";
 
+>>>>>>> e2f72fd0517045426abbebdb75487056f5253db1
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Language/>
+        <Navbar/>
+        <Hero/>
+        <About/>
+        <Language/>
+        <Footer />
     </div>
   )
 }
