9281bd83aaa9467c248d6a0eabf98b3f3b628291
gift kateule <giftkateule719@gmail.com>
2024-08-26 12:08:23 +0200

    my work

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
-<<<<<<< HEAD
 import Social from './Components/Social/Social';
 import Products from './Components/Products/Products';
 import Workstation from './Components/Workstation/Workstation'
 import Footer from './Components/Footer';
 import Contact from './Components/Contact/Contact';
-=======
-import Language from "./Components/Social/Social.jsx";
-import Footer from "./Footer.jsx";
->>>>>>> 26f2cf0846f6059ad7e08abdc70ddbdce01d4879
+
 
 
 const App = () => {
@@ -19,14 +15,10 @@ const App = () => {
         <Navbar/>
         <Hero/>
         <About/>
-<<<<<<< HEAD
         <Products/>
         <Social/>
         <Workstation/>
         <Contact/>
-=======
-        <Language/>
->>>>>>> 26f2cf0846f6059ad7e08abdc70ddbdce01d4879
         <Footer />
     </div>
   )
